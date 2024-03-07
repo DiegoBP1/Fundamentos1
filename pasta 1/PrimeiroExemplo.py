bc427665b0db9e6d4123e54b57061d507e9caec3
DiegoBP1 <diegopivotto18@outlook.com>
2024-03-06 21:43:43 -0300

    Create PrimeiroExemplo.py

---
+++ pasta 1/PrimeiroExemplo.py
@@ -0,0 +1,24 @@
+# Comando de saida
+print('Hello World')
+
+# criando uma variável numérica e atribuindo um valor
+
+num = 10 
+
+# Imprimindo o valor da variável num
+
+print(num)
+
+# Trocando o valor da variavel num
+
+num = -1
+
+print(num)
+
+nome =input('Digte seu nome: ')
+
+idade = int(input('Digite sua idade: '))
+
+print('Seu nome é',nome, 'e sua idade é',idade)
+
+
