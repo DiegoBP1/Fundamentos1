e1f1843130fd6a8675b8d7e91d579e4e65f0c20e
DiegoBP1 <diegopivotto18@outlook.com>
2024-04-10 22:00:00 -0300

    ExLista3

---
+++ Lista 3/Ex Lista3.py
@@ -0,0 +1,48 @@
+#numero1 = float (input('Digite um número: '))
+#numero2 = float (input('Digite mais um número: '))
+
+#divisao = numero1 / numero2
+
+#if numero2 == 0 or numero1 == 0:
+    #print('Digite um número diferente de 0!')
+
+#else:
+    #print('A divisão entre esses números é igual a',divisao)
+
+ 
+##--------------------------------------------------------------------------------
+
+
+#valorA = float (input('Digite o valor de A: '))
+
+#valorB = float (input('Digite o valor de B: '))
+
+#valorC = float (input('Digite o valor de C: '))
+
+#if valorA + valorB < valorA + valorC:
+    #print('A soma entre',valorA,'e',valorB,'é menor que',valorA,'+',valorC)
+
+#else:
+    #print('')
+
+#-------------------------------------------------------------------------------------
+
+numero = float (input('Digite um número: '))
+
+dobro = numero * 2
+triplo = numero* 3
+
+if numero > 0:
+    print('O dobro desse número é igual a',dobro)
+
+else:
+    print('O triplo desse número é igual a',triplo)
+
+
+
+    
+    
+
+
+
+   
